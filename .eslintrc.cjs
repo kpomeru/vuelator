f1d2df2a563672b90fe8c3ab4f9a993fc52590a3
Akpoteheri Omeru <kpomeru@gmail.com>
2022-12-29 15:40:25 +0100

    Scaffold and minor setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  //
+  root: true,
+  // eslint-disable-next-line prettier/prettier
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  indent: ["error", 4],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        tabWidth: 4,
+        useTabs: true,
+      },
+    ],
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam, sunt.
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
